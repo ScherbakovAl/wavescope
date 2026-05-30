eabd412d9833613c7482528ca8c33e753ab9c0dd
Scherbakov.al <scherbakov.al@gmail.com>
2026-05-31 01:50:16 +0300

    изменение для мультиплатформенности, но без сборок ещё

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let cuda_root = std::env::var("CUDA_ROOT")
-        .unwrap_or_else(|_| "/usr/local/cuda-13.1".to_string());
-    println!("cargo:rustc-link-search=native={}/lib64", cuda_root);
-    println!("cargo:rustc-link-lib=dylib=cuda");
-    println!("cargo:rustc-link-lib=dylib=cufft");
-    println!("cargo:rerun-if-env-changed=CUDA_ROOT");
-}
